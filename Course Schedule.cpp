bfe9ec2caebe1c8881c2f124aacc0372db4e6fd6
Ankit Kumar Jha <48297878+Mental-bicep@users.noreply.github.com>
2022-04-20 14:28:03 +0530

    Create Course Schedule.cpp
    added some code

---
+++ Course Schedule.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool kahns(int n,vector<vector<int>> &graph)
+{
+    vector<int> indegree(n,0);
+    // for(int i=0;i<graph.size();++i)
+        // for(int ele:graph[i]) ++indegree[ele];
+        vector<vector<int>> adj(n);
+        for(auto &ele:graph) {++indegree[ele[1]];
+            adj[ele[0]].push_back(ele[1]);                     
+        }
+        
+        queue<int> q;
+    for(int i=0;i<indegree.size();++i) 
+        if(indegree[i]==0) q.push(i);
+    vector<int> ans;
+    while(q.size())
+    { 
+        // cout<<"inside loop\n";
+        int rem=q.front();q.pop();
+        ans.push_back(rem);
+        for(auto &ele:adj[rem])
+        {
+            --indegree[ele];
+            if(indegree[ele]==0) q.push(ele);
+        }
+    }
+    if(ans.size()!=indegree.size()) {
+        // cout<<"cycle\n";  
+        return false;
+    }
+    return true ;
+}
+    
+    
+    bool canFinish(int n, vector<vector<int>>& graph) {
+           return kahns(n,graph);
+    }
+};
